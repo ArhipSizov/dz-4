1101f9bfcc23ffc1aba19159de98650f77a004dc
ArhipSizov <arhip.sizov111@gmail.com>
2023-11-29 18:41:06 +0300

    first commit

--- src/Burger/BigOneBurger/Burger.jsx
+++ src/Burger/BigOneBurger/Burger.jsx
@@ -1,66 +1,81 @@
-import './Burger.css';
+import "./Burger.css";
 import { useState } from "react";
-import closeImg from '../../../public/close.svg'
+import closeImg from "../../../public/close.svg";
 
-export default function Burger({img,name,weight,cost,id, showComponent, setShowComponent}){
+export default function Burger({
+  img,
+  name,
+  weight,
+  cost,
+  id,
+  showComponent,
+  setShowComponent,
+}) {
+  console.log(showComponent, name, img);
 
-console.log(showComponent,name,img);
+  const [num, setNum] = useState(0);
 
-    const [num,setNum] = useState(0)
-
-    function editBut(amper) {
-        if (String(amper) === "-1" && num === 0){
-          console.log(32);
-            return
-        }
-      const result = num + amper
-      setNum(result)
-      console.log(result);
+  function editBut(amper) {
+    if (String(amper) === "-1" && num === 0) {
+      console.log(32);
+      return;
     }
-    function setComponent(){
-      setShowComponent(false);
-      console.log(446);
+    const result = num + amper;
+    setNum(result);
+    console.log(result);
   }
-  function setComponent2(){
+  function setComponent() {
+    setShowComponent(false);
+    console.log(446);
+  }
+  function setComponent2() {
     setShowComponent(true);
     console.log(446);
-}
+  }
 
-    return(
-      showComponent && 
-    <div className="bigBurger">
-      <div className='notSoBigBurger'>
-        <p>{name}</p>
-        <img src={closeImg} onClick={setComponent} alt="" />
-        <div>
-          <img src={img} alt="" />
-          <div className="info">
-            <p>
-              Супер мясное наслаждение! Большая рубленая котлета из свежего
-              говяжего мяса покорит вас своей сочностью, а хрустящие листья салата
-              добавят свежести.
-            </p>
-            <div>
-            <p>хлеб</p>
-            <p>котлета</p>
-            <p>кетчуп</p>
-            <div>
-              <p>{weight}г,ккал 999</p>
-            </div>
-            </div>
+  return (
+    showComponent && (
+      <div className="bigBurger">
+        <div className="notSoBigBurger">
+          <div>
+            <p>{name}</p>
+            <img src={closeImg} onClick={setComponent} alt="" />
           </div>
-        </div>
-        <div>
-          <button></button>
-              <div className='buttonDiv'>
-              <button  className='invisBut' onClick={() => editBut(-1)}>-</button>
-                  <p className='num'>{num}</p>
-                  <button className='invisBut' onClick={() => editBut(+1)}>+</button>
+          <div>
+            <img src={img} alt="" />
+            <div className="info">
+              <p>
+                Супер мясное наслаждение! Большая рубленая котлета из свежего
+                говяжего мяса покорит вас своей сочностью, а хрустящие листья
+                салата добавят свежести.
+              </p>
+              <div className="flex">
+                <div className="flex">
+                  <p>Состав</p>
+                  <p>хлеб</p>
+                  <p>котлета</p>
+                  <p>кетчуп</p>
+                </div>
+                <div className="block">
+                  <p>{weight}г,ккал 999</p>
+                </div>
               </div>
+            </div>
           </div>
+          <div>
+            <button></button>
+            <div className="buttonDiv">
+              <button className="invisBut" onClick={() => editBut(-1)}>
+                -
+              </button>
+              <p className="num">{num}</p>
+              <button className="invisBut" onClick={() => editBut(+1)}>
+                +
+              </button>
+            </div>
           </div>
         </div>
-    
-
-    );
+      </div>
+    )
+  );
 }
